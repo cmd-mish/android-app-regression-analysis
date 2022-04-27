f893d34af9cadb159daf76e4b2e413880a29980a
popovmik-arcada <popovmik@arcada.fi>
2022-04-28 00:03:39 +0300

    Gränssnitt (v0.1) + r-uträkning

--- app/src/main/java/fi/arcada/regressionsanalys/MainActivity.java
+++ app/src/main/java/fi/arcada/regressionsanalys/MainActivity.java
@@ -10,8 +10,24 @@ public class MainActivity extends AppCompatActivity {
     // Två datamängder med längd och skonummer för 26 olika personer
     // hänger ihop på så vis att xData[0] och yData[0] är skonummer och längd  för den första personen, osv.
     // Observera att de är primitiva arrays, inte ArrayLists, så de behandlas lite annorlunda.
-    double[] xData = { 47,  42,  43,  42,  41,  48,  46,  44,  42,  43,  39,  43,  39,  42,  44,  45,  43,  44,  45,  42,  43,  32,  48,  43,  45,  45};
-    double[] yData = { 194, 188, 181, 177, 182, 197, 179, 176, 177, 188, 164, 171, 170, 180, 171, 185, 179, 182, 180, 178, 178, 148, 197, 183, 179, 198};
+    //double[] xData = { 47,  42,  43,  42,  41,  48,  46,  44,  42,  43,  39,  43,  39,  42,  44,  45,  43,  44,  45,  42,  43,  32,  48,  43,  45,  45};
+    //double[] yData = { 194, 188, 181, 177, 182, 197, 179, 176, 177, 188, 164, 171, 170, 180, 171, 185, 179, 182, 180, 178, 178, 148, 197, 183, 179, 198};
+
+    double[] xData = { 520,
+            610,
+            840,
+            540,
+            360,
+            260,
+            85 };
+
+    double[] yData = { 17,
+            22,
+            26,
+            19,
+            16,
+            15,
+            14 };
 
     // Deklarera yValue för längd, Denna variabel ska sedan få ett värde som hämtas från en EditText-box i appens GUI
     double yValue;
@@ -24,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Här kommer som vanligt alla findViewById som behövs
+        RegressionLine.rCalculate(xData, yData);
     }
 
     // Gör så att den här metoden anropas vid ett knapptryck
--- app/src/main/java/fi/arcada/regressionsanalys/RegressionLine.java
+++ app/src/main/java/fi/arcada/regressionsanalys/RegressionLine.java
@@ -2,6 +2,120 @@ package fi.arcada.regressionsanalys;
 
 public class RegressionLine {
 
+    double[] xData, yData;
+    double k, m, x, r;
+
+    public RegressionLine(double[] xData, double[] yData) {
+        this.xData = xData;
+        this.yData = yData;
+    }
+
+    public static void rCalculate(double[] xData, double[] yData) {
+        /* PREPARE DATA */
+        int n;
+        double Sx, Sy, Sxy, Sx2, Sy2;
+        double[] xy, x2, y2;
+
+        // n
+        n = xData.length;
+        System.out.println("n " + n);
+
+        // Sx
+        Sx = 0.0;
+        for (double value: xData) {
+            Sx += value;
+        }
+        System.out.println("Sx " + Sx);
+
+        // Sy
+        Sy = 0.0;
+        for (double value: yData) {
+            Sy += value;
+        }
+        System.out.println("Sy " + Sy);
+
+        // xy
+        xy = new double[n];
+        for (int i = 0; i < n; i++) {
+            xy[i] = xData[i] * yData[i];
+        }
+
+        // x2
+        x2 = new double[n];
+        for (int i = 0; i < n; i++) {
+            x2[i] = Math.pow(xData[i], 2);
+        }
+
+        // y2
+        y2 = new double[n];
+        for (int i = 0; i < n; i++) {
+            y2[i] = Math.pow(yData[i], 2);
+        }
+
+        // Sxy
+        Sxy = 0.0;
+        for (double value: xy) {
+            Sxy += value;
+        }
+        System.out.println("Sxy " + Sxy);
+
+        // Sx2
+        Sx2 = 0.0;
+        for (double value: x2) {
+            Sx2 += value;
+        }
+        System.out.println("Sx2 " + Sx2);
+
+        // Sy2
+        Sy2 = 0.0;
+        for (double value: y2) {
+            Sy2 += value;
+        }
+        System.out.println("Sy2 " + Sy2);
+
+        /* CALCULATE FORMULA in 11 steps (s1 - s11) */
+
+        // n(Sxy)
+        double s1 = n * Sxy;
+
+        // (Sx)(Sy)
+        double s2 = Sx * Sy;
+
+        // n(Sxy) - (Sx)(Sy)
+        double s3 = s1 - s2;
+
+        // n * Sx2
+        double s4 = n * Sx2;
+
+        // n * Sy2
+        double s5 = n * Sy2;
+
+        // (Sx)2
+        double s6 = Math.pow(Sx, 2);
+
+        // (Sy)2
+        double s7 = Math.pow(Sy, 2);
+
+        // (n * Sx2) - (Sx)2
+        double s8 = s4 - s6;
+
+        // (n * Sy2) - (Sy)2
+        double s9 = s5 - s7;
+
+        // [(n * Sx2) - (Sx)2] * [(n * Sy2) - (Sy)2]
+        double s10 = s8 * s9;
+
+        // sqrt s10
+        double s11 = Math.sqrt(s10);
+
+        // r
+        double r = s3 / s11;
+
+        System.out.println("r " + r);
+    }
+
+
+
     // deklarera k, m, x  och correlationCoefficient som double
 
     // Skapa en konstruktor som tar emot data-arrays för x och y
